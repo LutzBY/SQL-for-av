4dfbc2ebeb9ed7e2ed3366cef098520914f5d6b3
LutzBY <lutzby@gmail.com>
2024-08-23 13:11:02 +0300

    modified:   "sql/select av_full \321\204\320\270\320\273\321\214\321\202\321\200 \320\277\320\276 \320\260\320\271\320\264\320\270.sql" new file:   sql/update av set exclude_flag.sql обновлено с учетом введения столбца exclude

--- sql/select av_full фильтр по айди.sql
+++ sql/select av_full фильтр по айди.sql
@@ -1,3 +1,4 @@
-SELECT id, price, model_vlk, brand, model, model_misc, year, type, cylinders, capacity
+SELECT id, price, model_vlk, brand, model, model_misc, year, type, cylinders, capacity, exclude_flag, url
 FROM public.av_full
-WHERE id = 110347242
+WHERE id = 110495074
+order by year asc
---
+++ sql/update av set exclude_flag.sql
@@ -0,0 +1,3 @@
+UPDATE public.av_full
+SET exclude_flag = True
+where id = 110495290;
