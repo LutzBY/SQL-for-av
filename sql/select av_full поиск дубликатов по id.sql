fc9976abe3cb70349ae92e97d4da8f887a8af1a9
LutzBY <lutzby@gmail.com>
2024-11-22 09:17:27 +0300

    modified:   "sql/select av_full \320\277\320\276\320\270\321\201\320\272 \320\264\321\203\320\261\320\273\320\270\320\272\320\260\321\202\320\276\320\262 \320\277\320\276 id.sql" логика отличается от автоматизированной для более эффективного поиска дубликатов

--- sql/select av_full поиск дубликатов по id.sql
+++ sql/select av_full поиск дубликатов по id.sql
@@ -1,10 +1,11 @@
-SELECT id, date, price, model_vlk, brand, model, model_misc, year, type, cylinders, capacity, mileage, exclude_flag, url
+SELECT *
 FROM public.av_full
-WHERE (brand, model, model_misc, year, type, cylinders, capacity, mileage, seller, locations) = 
+WHERE (brand, model, year, type, cylinders, capacity, seller, locations) = 
 (
-    SELECT brand, model, model_misc, year, type, cylinders, capacity, mileage, seller, locations
+    SELECT brand, model, year, type, cylinders, capacity, seller, locations
     FROM public.av_full
-    WHERE id = 112262309
+    WHERE id = 112944635
+	-- model_vlk, mileage
 )
 --AND id != 105787706
 ORDER BY date ASC;
