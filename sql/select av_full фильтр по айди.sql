9d715f9aa88d27a293dd312bd14e892103462daa
LutzBY <lutzby@gmail.com>
2025-03-18 10:22:00 +0300

    modified:   "sql/select av_full \321\204\320\270\320\273\321\214\321\202\321\200 \320\277\320\276 \320\260\320\271\320\264\320\270.sql" Добавлены новые поля

--- sql/select av_full фильтр по айди.sql
+++ sql/select av_full фильтр по айди.sql
@@ -1,4 +1,4 @@
-SELECT id, price, model_vlk, brand, model, model_misc, year, type, cylinders, capacity, exclude_flag, url, date, date_corrected, duplicate_flag, duplicate_id
-FROM public.av_full
-WHERE id in (114597106, 110903324)
+SELECT id, price, model_vlk, brand, model, model_misc, year, type, cylinders, capacity, exclude_flag, url, date, date_corrected, duplicate_flag, duplicate_id, seller, locations, seller_ph_nr, status, status_date 
+FROM public.av_full 
+WHERE id in (115278131, 110279690)
 order by id asc
