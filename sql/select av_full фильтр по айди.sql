960ff6b693a245839efcd5659ebe7bd8077aa53b
LutzBY <lutzby@gmail.com>
2025-03-31 10:53:15 +0300

    modified:   "sql/select av_full \321\204\320\270\320\273\321\214\321\202\321\200 \320\277\320\276 \320\260\320\271\320\264\320\270.sql"

--- sql/select av_full фильтр по айди.sql
+++ sql/select av_full фильтр по айди.sql
@@ -1,4 +1,5 @@
-SELECT id, price, model_vlk, brand, model, model_misc, year, type, cylinders, capacity, exclude_flag, url, date, date_corrected, duplicate_flag, duplicate_id, seller, locations, seller_ph_nr, status, status_date 
+SELECT id, price, model_vlk, brand, model, model_misc, year, type, cylinders, capacity, exclude_flag, url, date, date_corrected, duplicate_flag, duplicate_id, seller, locations, seller_ph_nr, status, status_date
 FROM public.av_full 
-WHERE id in (115278131, 110279690)
+WHERE id = 115605813
+--or id in ()
 order by id asc
