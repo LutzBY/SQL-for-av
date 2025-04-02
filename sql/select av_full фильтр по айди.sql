5e737ad034e2ce065c0371101954e5e5c0c3c399
LutzBY <lutzby@gmail.com>
2025-04-02 09:02:08 +0300

    modified:   "sql/select av_full \321\204\320\270\320\273\321\214\321\202\321\200 \320\277\320\276 \320\260\320\271\320\264\320\270.sql" В основной запрос добавлена связть с av_price_change

--- sql/select av_full фильтр по айди.sql
+++ sql/select av_full фильтр по айди.sql
@@ -1,5 +1,8 @@
-SELECT id, price, model_vlk, brand, model, model_misc, year, type, cylinders, capacity, exclude_flag, url, date, date_corrected, duplicate_flag, duplicate_id, seller, locations, seller_ph_nr, status, status_date
-FROM public.av_full 
-WHERE id = 115605813
---or id in ()
+SELECT af.id, price, string_agg(aph.usd_eq::text, ', ') as pr_ch, model_vlk, brand, model, model_misc, year, type, cylinders, capacity, url, af.date, date_corrected, duplicate_flag, duplicate_id, seller, locations, seller_ph_nr, status, status_date, exclude_flag
+FROM public.av_full af
+LEFT JOIN av_price_history aph
+ON af.id = aph.id_av
+WHERE af.id = 115624314
+--or af.id in (115371199)
+group by af.id
 order by id asc
